4f566514e353034877f49a83876304d6351a4156
AstrakhantsevaAA <astra92293@gmail.com>
2022-09-26 14:06:34 +0200

    [feature] add inference func, move eval to helpers

--- captcha/training/train.py
+++ captcha/training/train.py
@@ -1,87 +1,18 @@
 from typing import Any, Optional
 
 import hydra
-import numpy as np
-import pandas as pd
 import torch
 import torch.optim as optim
 from clearml import Logger, Task
 from omegaconf import DictConfig
-from sklearn.metrics import accuracy_score
 from torch import nn
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 
+from captcha.api.helpers import evaluation
 from captcha.config import net_config, system_config, torch_config
 from captcha.nets.define_net import define_net
 from captcha.training.train_utils import Phase, create_dataloader, fix_seeds
-
-
-def evaluation(
-    model,
-    eval_dataloader: DataLoader,
-    criterion: Any,
-    epoch: int,
-    logger: Optional[Logger] = None,
-):
-    preds_collector = []
-    model.eval()
-    running_loss = 0.0
-    iters = len(eval_dataloader)
-
-    print(f"Starting validation epoch {epoch}")
-    with torch.no_grad():
-        for batch in tqdm(eval_dataloader, total=len(eval_dataloader)):
-            logits = model.forward(batch["image"].to(torch_config.device))
-            probs = nn.functional.softmax(logits, dim=1)
-            probs = probs.cpu().detach().numpy()
-
-            batch_size = len(batch["image"])
-            preds_batch = np.zeros((batch_size, net_config.LEN_CAPTCHA))
-            labels_batch = np.zeros((batch_size, net_config.LEN_CAPTCHA))
-            for i in range(net_config.LEN_CAPTCHA):
-                start, end = (
-                    i * net_config.LEN_SYMBOLS,
-                    (i + 1) * net_config.LEN_SYMBOLS,
-                )
-                pred_max_idxs = np.argmax(probs[:, start:end], axis=1)
-                label_max_ids = np.argmax(batch["label"][:, start:end], axis=1)
-                for j in range(batch_size):
-                    preds_batch[j, i] = pred_max_idxs[j]
-                    labels_batch[j, i] = label_max_ids[j]
-
-            preds_df = pd.DataFrame(
-                preds_batch.tolist(),
-                columns=[f"preds_{k}" for k in range(net_config.LEN_CAPTCHA)],
-            )
-            labels_df = pd.DataFrame(
-                labels_batch.tolist(),
-                columns=[f"labels_{k}" for k in range(net_config.LEN_CAPTCHA)],
-            )
-            preds_collector.append(pd.concat([preds_df, labels_df], axis=1))
-
-            loss = criterion(logits, batch["label"].to(torch_config.device))
-            running_loss += loss.item()
-
-    eval_preds_df = pd.concat(preds_collector, ignore_index=True)
-    accuracy = 0
-    for l in range(net_config.LEN_CAPTCHA):
-        accuracy += accuracy_score(
-            eval_preds_df.iloc[:, l].values,
-            eval_preds_df.iloc[:, net_config.LEN_CAPTCHA + l].values,
-        )
-    print(accuracy / net_config.LEN_CAPTCHA)
-
-    if logger is not None:
-        logger.report_scalar(
-            f"Accuracy",
-            "eval",
-            iteration=epoch,
-            value=accuracy / net_config.LEN_CAPTCHA,
-        )
-        logger.report_scalar(
-            f"Loss", "eval", iteration=epoch, value=running_loss / iters
-        )
 
 
 def train_one_epoch(
@@ -143,13 +74,14 @@ def train_model(cfg: DictConfig):
         loss = train_one_epoch(
             model, dataloader[Phase.train], optimizer, criterion, epoch, logger
         )
-        evaluation(model, dataloader[Phase.val], criterion, epoch, logger)
-    if cfg.train.model_save_path:
-        model_save_path = system_config.model_dir / cfg.train.model_save_path
-        model_save_path.mkdir(exist_ok=True)
-        torch.save(
-            model, system_config.model_dir / cfg.train.model_save_path / "model.pth"
-        )
+        _ = evaluation(model, dataloader[Phase.val], criterion, epoch, logger)
+        if cfg.train.model_save_path:
+            model_save_path = system_config.model_dir / cfg.train.model_save_path
+            model_save_path.mkdir(exist_ok=True, parents=True)
+            print(f"Saving model to {model_save_path} as model.pth")
+            torch.save(
+                model, system_config.model_dir / cfg.train.model_save_path / "model.pth"
+            )
 
     if logger is not None:
         logger.flush()
