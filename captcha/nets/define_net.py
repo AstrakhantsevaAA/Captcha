04ce2ee04d377e9b6149aa66477c01d878b517a9
AstrakhantsevaAA <astra92293@gmail.com>
2022-09-26 14:08:32 +0200

    [feature] load trained model for inference

--- captcha/nets/define_net.py
+++ captcha/nets/define_net.py
@@ -1,10 +1,18 @@
+from typing import Optional
+
+import torch
 import torchvision.models as models
 from torch import nn
 
 from captcha.config import torch_config
 
 
-def define_net(freeze_grads: bool = False, outputs: int = 1, pretrained: bool = False):
+def define_net(
+    freeze_grads: bool = False,
+    outputs: int = 1,
+    pretrained: bool = False,
+    weights: Optional = None,
+):
     pretrained_weights = models.ResNet18_Weights.DEFAULT if pretrained else None
     model = models.resnet18(weights=pretrained_weights)
 
@@ -15,6 +23,10 @@ def define_net(freeze_grads: bool = False, outputs: int = 1, pretrained: bool = 
     model.fc = nn.Linear(
         in_features=model.fc.in_features, out_features=outputs, bias=True
     )
+
+    if weights is not None:
+        model = torch.load(weights, map_location="cpu")
+
     model.to(torch_config.device)
 
     return model
