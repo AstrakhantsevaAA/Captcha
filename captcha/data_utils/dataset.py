911109733b38d0500d85a7eee631859e33419220
AstrakhantsevaAA <astra92293@gmail.com>
2022-10-01 17:31:12 +0200

    [feature] add more augmentations, del imagenet normalization, add image preprocess

--- captcha/data_utils/dataset.py
+++ captcha/data_utils/dataset.py
@@ -1,26 +1,26 @@
 from pathlib import Path
-from typing import Optional
 
+import cv2
 import numpy as np
 import pandas as pd
 import torch
 from albumentations import (
+    Affine,
     Blur,
     CoarseDropout,
     Compose,
     Downscale,
-    HorizontalFlip,
-    OneOf,
-    RandomRotate90,
-    Rotate,
+    GridDistortion,
+    PadIfNeeded,
+    Perspective,
     ShiftScaleRotate,
-    VerticalFlip,
 )
 from PIL import Image
 from torch.utils.data import Dataset
 from torchvision import transforms
 
 from captcha.config import net_config
+from captcha.data_utils.preprocessing import image_preprocess
 
 
 def one_hot(letter: str) -> list:
@@ -32,61 +32,93 @@ def one_hot(letter: str) -> list:
 
 class CaptchaDataset(Dataset):
     """Reads in an image, transforms pixel values, and serves
-    a dictionary containing the image id, image tensors, and label.
+    a dictionary containing the image tensors, and label.
     """
 
     def __init__(
         self,
         data_dir: Path,
         x_df: pd.DataFrame,
-        y_df: Optional[pd.DataFrame] = None,
         augmentations_intensity: float = 0.0,
         test_size: int = 0,
     ):
         self.data_dir = data_dir
         self.data = x_df if test_size <= 0 else x_df.iloc[:test_size]
-        self.labels = y_df if test_size <= 0 else y_df.iloc[:test_size]
+        if "label" in self.data.columns:
+            self.labels = self.data["label"].astype(str)
         self.transform = transforms.Compose(
             [
                 transforms.Resize((224, 224)),
                 transforms.ToTensor(),
-                transforms.Normalize(
-                    mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)
-                ),
             ]
         )
         if augmentations_intensity > 0:
             self.augmentation = Compose(
                 [
-                    OneOf(
-                        [HorizontalFlip(), VerticalFlip()],
-                        p=0.1,
+                    CoarseDropout(
+                        max_height=3,
+                        max_width=3,
+                        p=0.99,
+                        min_holes=20,
+                        max_holes=40,
+                        fill_value=255,
                     ),
-                    OneOf(
-                        [Rotate(p=1.0, limit=30), RandomRotate90(p=1.0)],
-                        p=0.1,
+                    CoarseDropout(
+                        max_height=3,
+                        max_width=3,
+                        p=0.99,
+                        min_holes=20,
+                        max_holes=100,
+                        fill_value=0,
                     ),
-                    ShiftScaleRotate(p=1.0, rotate_limit=30),
                     Blur(blur_limit=3, p=0.1),
-                    CoarseDropout(max_height=7, max_width=7, p=0.1),
+                    ShiftScaleRotate(p=1.0, rotate_limit=20),
+                    Affine(),
+                    GridDistortion(border_mode=cv2.BORDER_CONSTANT, value=0),
                     Downscale(
                         scale_min=0.6,
                         scale_max=0.9,
                         p=0.7,
                     ),
+                    Perspective(p=0.9),
+                    PadIfNeeded(
+                        min_height=180,
+                        min_width=200,
+                        always_apply=True,
+                        border_mode=cv2.BORDER_CONSTANT,
+                        value=0,
+                    ),
                 ],
                 p=augmentations_intensity,
             )
         else:
-            self.augmentation = None
+            self.augmentation = Compose(
+                [
+                    PadIfNeeded(
+                        min_height=180,
+                        min_width=200,
+                        always_apply=True,
+                        border_mode=cv2.BORDER_CONSTANT,
+                        value=0,
+                    ),
+                ]
+            )
 
     def __getitem__(self, index):
-        image = Image.open(self.data.iloc[index]).convert("RGB")
+        filepath = str(self.data["filepath"].iloc[index])
+        image = cv2.imread(filepath)
+
+        if image is None:
+            raise Exception(
+                f"image is None, got filepath: {filepath} \n data: {self.data}"
+            )
+
+        image = image_preprocess(image)
+
         if self.augmentation is not None:
-            image = self.augmentation(image=np.array(image))["image"]
-            image = self.transform(Image.fromarray(image))
-        else:
-            image = self.transform(image)
+            image = self.augmentation(image=image)["image"]
+
+        image = self.transform(Image.fromarray(image))
 
         if self.labels is None:
             sample = {"image": image}
@@ -95,8 +127,16 @@ class CaptchaDataset(Dataset):
             label = self.labels.iloc[index]
             for l in label:
                 oh_label += one_hot(l)
-            label = torch.tensor(oh_label, dtype=torch.float)
-            sample = {"image": image, "label": label}
+            label_encode = torch.tensor(oh_label, dtype=torch.float)
+            assert (
+                len(label_encode) == net_config.LEN_TOTAL
+            ), f"{len(label_encode)}, {label}"
+            sample = {
+                "image": image,
+                "label": label_encode,
+                "label_decode": label,
+                "filepath": filepath,
+            }
 
         return sample
 
