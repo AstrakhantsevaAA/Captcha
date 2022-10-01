66d86ef3bd2fa8e61f0508a5474571f1ff170cb1
AstrakhantsevaAA <astra92293@gmail.com>
2022-10-01 17:28:22 +0200

    [feature] trying to remove noize and to binarize

---
+++ captcha/data_utils/preprocessing.py
@@ -0,0 +1,13 @@
+import cv2
+import numpy as np
+
+
+def image_preprocess(image: np.ndarray) -> np.ndarray:
+    im_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    ret, im_inv = cv2.threshold(im_gray, 220, 255, cv2.THRESH_BINARY_INV)
+    kernel = 1 / 16 * np.array([[1, 2, 1], [2, 4, 2], [1, 2, 1]])
+    im_blur = cv2.filter2D(im_inv, -1, kernel)
+    ret, im_res = cv2.threshold(im_blur, 150, 255, cv2.THRESH_BINARY)
+    # preprocessed_image = cv2.cvtColor(im_res,cv2.COLOR_GRAY2BGR)
+
+    return im_res
