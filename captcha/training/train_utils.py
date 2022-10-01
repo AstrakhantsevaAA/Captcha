f162e9249ed2c13d250d448050c6fe43ff065dc3
AstrakhantsevaAA <astra92293@gmail.com>
2022-10-01 17:34:51 +0200

    [feature] del splitting set on train and val in code,

--- captcha/training/train_utils.py
+++ captcha/training/train_utils.py
@@ -8,7 +8,6 @@ from typing import Optional
 import numpy as np
 import pandas as pd
 import torch
-from sklearn.model_selection import train_test_split
 from torch.utils.data import DataLoader
 
 from captcha.config import system_config
@@ -31,6 +30,8 @@ def fix_seeds(random_state: int = 42):
 
 
 def create_dataloader(
+    train_path: Optional[str] = None,
+    eval_path: Optional[str] = None,
     augmentations_intensity: float = 0,
     batch_size: int = 32,
     test_size: int = 0,
@@ -39,33 +40,43 @@ def create_dataloader(
 ):
     dataloader = defaultdict()
 
-    if files is None:
-        data_path = Path(
-            "/home/alenaastrakhantseva/PycharmProjects/Captcha/data/raw/source6"
-        )
-        files = sorted(data_path.rglob("*.png"))
-
-    df = pd.DataFrame({"filepath": files})
-    df["label"] = df["filepath"].apply(lambda x: x.stem)
-
     if inference:
-        dataset = CaptchaDataset(system_config.data_dir, df["filepath"], df["label"])
+        if files is None:
+            raise Exception("files is None, for inference files are necessary")
+        df = pd.DataFrame({"filepath": files})
+        df["label"] = df["filepath"].apply(lambda x: Path(x).stem)
+        dataset = CaptchaDataset(system_config.data_dir, df)
+
         return DataLoader(dataset, batch_size=batch_size)
 
-    x_train, x_eval, y_train, y_eval = train_test_split(
-        df["filepath"], df["label"], test_size=0.25, random_state=42, shuffle=True
+    if train_path is None or eval_path is None:
+        raise Exception(
+            "csv files with train and eval data are None, for training those files are necessary"
+        )
+
+    data = pd.read_csv(
+        system_config.data_dir / train_path, index_col=0, dtype={"label": str}
+    )
+    data_eval = pd.read_csv(
+        system_config.data_dir / eval_path, index_col=0, dtype={"label": str}
+    )
+    data_test = pd.read_csv(
+        system_config.data_dir / "processed/test_set.csv", dtype={"label": str}
     )
 
     shuffle = True
     for phase in Phase:
         if phase == Phase.val:
+            augmentations_intensity, shuffle = 0.9, False
+            data = data_eval
+
+        if phase == Phase.test:
             augmentations_intensity, shuffle = 0.0, False
-            x_train, y_train = x_eval, y_eval
+            data = data_test
 
         dataset = CaptchaDataset(
             system_config.data_dir,
-            x_train,
-            y_train,
+            data,
             augmentations_intensity,
             test_size=test_size,
         )
