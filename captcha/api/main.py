dec3309016f6f2873a823219f7d69a227cbbebbe
AstrakhantsevaAA <astra92293@gmail.com>
2022-10-04 22:43:29 +0200

    [feature] api got bytes instead of paths (str), works only for one image

--- captcha/api/main.py
+++ captcha/api/main.py
@@ -1,7 +1,8 @@
 from enum import Enum
+from pathlib import Path
 from typing import List, Optional
 
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile
 from pydantic import BaseModel, ValidationError
 
 from captcha.api.helpers import inference
@@ -18,7 +19,7 @@ class APIResponse(BaseModel):
 
 
 class Data(BaseModel):
-    image_paths: List[str]
+    images: list[UploadFile]
 
 
 class EndpointName(str, Enum):
@@ -28,14 +29,39 @@ class EndpointName(str, Enum):
 app = FastAPI()
 
 
-@app.post("/endpoints/{endpoint_name}")
-async def get_model(endpoint_name: EndpointName, data: Data):
+@app.get("/health_check")
+def health_check():
+    return APIResponse(endpoint_name="health_check", message="OK")
+
+
+@app.post("/{endpoint_name}")
+async def get_model(endpoint_name: EndpointName, data: UploadFile):
+    data = check_data([data])
     external_data = {}
     if endpoint_name == EndpointName.classification:
-        external_data = inference(data.image_paths)
+        external_data = inference(data)
     try:
         response = APIResponse(**external_data)
     except ValidationError as e:
         response = APIResponse(message=str(e))
 
     return response
+
+
+def check_data(files):
+    new_paths = []
+    for file in files:
+        try:
+            contents = file.file.read()
+            new_filepath = Path("/tmp/captcha")
+            new_filepath.mkdir(parents=True, exist_ok=True)
+            print(new_filepath / file.filename)
+            with open(new_filepath / file.filename, "wb") as f:
+                f.write(contents)
+            new_paths.append(new_filepath / file.filename)
+        except Exception as e:
+            return {"message": f"There was an error uploading the file(s).\nError: {e}"}
+        finally:
+            file.file.close()
+
+    return new_paths
