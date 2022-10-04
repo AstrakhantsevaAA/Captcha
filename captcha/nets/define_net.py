3fb7c0b607747f487ade76b6ce4d9a3b90068618
AstrakhantsevaAA <astra92293@gmail.com>
2022-10-04 22:39:57 +0200

    [fix] fix load net

--- captcha/nets/define_net.py
+++ captcha/nets/define_net.py
@@ -13,20 +13,20 @@ def define_net(
     pretrained: bool = False,
     weights: Optional = None,
 ):
-    pretrained_weights = models.ResNet18_Weights.DEFAULT if pretrained else None
-    model = models.resnet18(weights=pretrained_weights)
-    model.conv1 = nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3, bias=False)
-
-    if freeze_grads:
-        for params in model.parameters():
-            params.requires_grad_ = False
-
-    model.fc = nn.Linear(
-        in_features=model.fc.in_features, out_features=outputs, bias=True
-    )
-
     if weights is not None:
         model = torch.load(weights, map_location="cpu")
+    else:
+        pretrained_weights = models.ResNet18_Weights.DEFAULT if pretrained else None
+        model = models.resnet18(weights=pretrained_weights)
+        model.conv1 = nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3, bias=False)
+
+        if freeze_grads:
+            for params in model.parameters():
+                params.requires_grad_ = False
+
+        model.fc = nn.Linear(
+            in_features=model.fc.in_features, out_features=outputs, bias=True
+        )
 
     model.to(torch_config.device)
 
